5421ad5e231691fb69e1b5800c963ec2b046a405
Etienne Detoisien <detoisienetienne@gmail.com>
2023-03-31 19:03:29 +0200

    add responsive and disgn for navbar

--- src/Ecommerce/static/js/base.js
+++ src/Ecommerce/static/js/base.js
@@ -1,6 +1,9 @@
 const hamburgerMenu = document.querySelector('.nav__hamburger-menu');
 const toggleMenu = document.querySelector('.toggle-menu')
+console.log(toggleMenu)
 hamburgerMenu.addEventListener('click', function() {
   this.classList.toggle('active');
   toggleMenu.classList.toggle('active');
+  console.log(this)
+  console.log(toggleMenu)
 });
